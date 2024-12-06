af9a9056ac9b88a53b160aa6a1c794d1668eaadd
fsonic202 <fsonic3@gmail.com>
2024-12-06 07:31:44 -0700

    update on process work

--- process.php
+++ process.php
@@ -1,5 +1,16 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo $_POST["email"];
+
+    function purgeDate($data){
+        $date = stripslashes($_POST["email"]);
+        $data = htmlspecialchars($data);
+        $data = trim($data);
+        return $data;
+    }
+   // echo htmlspecialchars($_POST["email"])."<br>";
+   // echo htmlspecialchars($_POST["pwd"]);
+   echo purgeDate($_POST["email"]). "<br>";
+   echo purgeDate($_POST["pwd"])."<br>";
 }
+
 ?>
