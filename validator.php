93781dee05f7cc4fb47fcc5c6bf1d7413bd9890e
fsonic202 <fsonic3@gmail.com>
2024-12-09 06:53:19 -0700

    added navbar with update code in validator

--- validator.php
+++ validator.php
@@ -1,8 +1,33 @@
 <?php
 class validator {
     //my errors verable
-$errors = [];
+private $errors = [];
 
+//method
+public function validateEmpty($value, $fieldName){
+    if(empty($value)) {
+        if($fieldName == "FullName"){
+            $this->errors[$fieldName] = "please enter full name";
+        }elseif($fieldName == "email"){
+            $this->errors[$fieldName] = "please enter email address";
+        }elseif($fieldName == "pwd"){
+            $this->errors[$fieldName] = "please enter password";
+        }elseif($fieldName == "confirmPwd"){
+            $this->errors[$fieldName] = "please enter confirm Password";
+        }else{
+            $this->error[$fieldName] = "field is required";
+        }
+    }
+}
+
+// to check if the registration has errors
+
+public function hasErrors(){
+    return !empty($this->errors);
+}
+public function geterrors(){
+    return $this->errors;
+}
 }
 
 ?>
